1a4c811e43ca8d63b2708190e1baad90f86e1612
HugoDF <hdifrancesco@gmail.com>
2016-10-07 09:41:34 +0100

    Remove unnecessary deps

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = {
-    'tfl': require('./lib/tfl'),
-
-    'bus': require('./lib/bus'),
-    'cycle': require('./lib/cycle'),
-    'overground': require('./lib/overground'),
-    'road': require('./lib/road'),
-    'tube': require('./lib/tube'),
+    tube: {
+      lines: require('./fixtures/lines.json'),
+      stations: require('./fixtures/stations.json')
+    }
+    // 'tfl': require('./lib/tfl'),
+    //
+    // 'bus': require('./lib/bus'),
+    // 'cycle': require('./lib/cycle'),
+    // 'overground': require('./lib/overground'),
+    // 'road': require('./lib/road'),
+    // 'tube': require('./lib/tube'),
 };
